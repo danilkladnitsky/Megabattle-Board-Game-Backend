95abfa5242312fc5fdb45e67fef74409e4e30e45
Danl Kladnitskiy <core2pixel@gmail.com>
2021-04-23 14:47:44 +0300

    Serving static files

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 const io = require('socket.io')();
 let cors = require("cors")
+app.use(express.static(path.join(__dirname,  "build")));
+app.use(express.static("public"));
 app.use(cors())
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 const port = 4000;
 app.io = io;
 require('./routes')(app, {});
